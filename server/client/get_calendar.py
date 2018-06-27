01da8ea612c46fc6ad653c5dd9b7a348dfc2fd60
Bradley Dettmer <11279144+redSlug@users.noreply.github.com>
2018-06-26 23:48:23 -0400

    events ordered by date, refactored

--- server/client/get_calendar.py
+++ server/client/get_calendar.py
@@ -6,63 +6,78 @@ import pytz
 
 SUMMARY_LIMIT = 25
 CALENDAR_DATA_LIMIT = 120
+MAX_EVENTS_SHOWN = 3
 CALENDAR_DATA = 'client/generated/calendar_data'
 
-def getEventsInFuture(calendar_token):
-    calendarURL = 'https://www.recurse.com/calendar/events.ics?token=' + calendar_token
-    result = requests.get(url=calendarURL)
-    output = result.text
-    titles = []
-    output = output.split("\n")
+
+class Event:
+    def __init__(self):
+        self.start_time = None
+        self.summary = None
+        self.status = None
+
+
+def get_line_contents(line):
+    return re.search('^.*:(.*)', line).group(1)
+
+
+def get_short_summary(s):
+    if len(s) <= SUMMARY_LIMIT:
+        return s
+    result = ''
+    for w in s.split():
+        if len(result + w) > SUMMARY_LIMIT - 3:
+            result += '...'
+            break
+        result += ' ' + w
+    return result
+
+
+def write_formatted_events(calendar_token):
+    calendar_url = 'https://www.recurse.com/calendar/events.ics?token=' + calendar_token
+    raw_events = requests.get(url=calendar_url).text.split('\n')
     now = datetime.datetime.now(pytz.timezone('US/Eastern'))
-    dateForEvent = None
-    isActiveEvent = False
-    for o in output:
-        if (isActiveEvent) and (o.startswith("SUMMARY")) and (not "office hours" in o.lower()):
-            o = o.replace("SUMMARY:","")
-            titles.append((o, dateForEvent.strftime('%H:%M')))
-            isActiveEvent = False
 
-        else:
-            '''when we find the start time'''
-            if o.startswith("DTSTART"):
-                dateLine =  re.search('^DTSTART.*:(.*)', o)
-                dateLine = dateLine.group(1)[:-1]
-                cal_time = datetime.datetime.strptime(dateLine, "%Y%m%dT%H%M%S")
-                time_zone = pytz.timezone('US/Eastern')
-                cal_time = time_zone.localize(cal_time)
-                if now < cal_time < now + datetime.timedelta(hours=3):
-                    isActiveEvent = True
-                    dateForEvent = cal_time
+    events = list()
+    event = None
+    for raw_event in raw_events:
+        raw_event = raw_event.strip()
+        if event and raw_event.startswith('SUMMARY'):
+            event.summary = get_line_contents(raw_event)
+        elif event and raw_event.startswith('STATUS'):
+            event.status = get_line_contents(raw_event)
+        elif event and raw_event.startswith('DTSTART'):
+            start_time = get_line_contents(raw_event)
+            start_time = datetime.datetime.strptime(start_time, "%Y%m%dT%H%M%S")
+            event.start_time = pytz.timezone('US/Eastern').localize(start_time)
+        elif raw_event.startswith('BEGIN:VEVENT'):
+            event = Event()
+        elif raw_event.startswith('END:VEVENT'):
+            events.append(event)
+            event = None
 
-    titles.sort(key=lambda x:x[1])
-    titles = [(summary.rstrip(),time) for summary,time in titles]
-    event_dictionary = {}
-    for summary, date in titles:
-        if len(summary) > SUMMARY_LIMIT:
-            summary_words = summary.split()
-            short_summary = ''
-            for word in summary_words:
-                if len(short_summary + word) + 4 > SUMMARY_LIMIT:
-                    break
-                short_summary += ' ' + word
-            summary = short_summary + '...'
+    # filter events
+    events = [e for e in events if e.summary]
+    events = [e for e in events if now < e.start_time < now + datetime.timedelta(hours=16)]
+    events = [e for e in events if e.status is not 'CANCELLED']
+    events = [e for e in events if 'office hours' not in e.summary.lower()]
 
-        if summary not in event_dictionary:
-            event_dictionary[summary] = date
-            
-    event_data = ' '
-    for (title, date) in event_dictionary.items():
-        if len(title + date) > CALENDAR_DATA_LIMIT:
-            continue
+    # make summaries short
+    for event in events:
+        event.summary = get_short_summary(event.summary)
 
-        event_data += '{title} {date}, '.format(title=title, date=date)
-    event_data = event_data[:-2]
+    events.sort(key=lambda x: x.start_time)
 
-    if len(event_dictionary):
-        event_data = ' Upcoming: ' + event_data
+    events_string = ''
+    for event in events[:MAX_EVENTS_SHOWN]:
+        summary = event.summary
+        time = event.start_time.strftime('%H:%M')
+        if len(events_string + summary + time) + 2 > CALENDAR_DATA_LIMIT:
+            break
+        events_string += '{summary} {time}, '.format(summary=summary, time=time)
+
     with open(CALENDAR_DATA, 'w') as f:
-        f.write(event_data)
+        f.write(events_string[:-2])
 
 if __name__ == "__main__":
-    getEventsInFuture(os.environ['CALENDAR_TOKEN'])
+    write_formatted_events(os.environ['CALENDAR_TOKEN'])
