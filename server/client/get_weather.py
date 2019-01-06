6be15782dceaf11bc16146ddb1a015e3f20bc74e
Brad Dettmer <11279144+redSlug@users.noreply.github.com>
2019-01-06 13:15:50 -0500

    create banner maker object

--- server/client/get_weather.py
+++ server/client/get_weather.py
@@ -52,65 +52,72 @@ class WeatherInfo:
         return dict(currently_icon=currently_icon, summary=summary)
 
 
-def replace_banner(currently_icon, summary):
+class BannerMaker:
 
-    calendar_text = get_calendar_data()
-    calendar_text+= "  "
-    message_text = get_message_data()
-    font_size_in_points = 9
-    font = ImageFont.truetype(FONTS_DIR + 'led.ttf', font_size_in_points)
-    font_size = font.getsize(summary)
-    print('summary: {}, font size: {} calendar_text: {}, message_text{}'.format(summary, font_size, calendar_text, message_text))
-    summary_img = Image.new('RGB', font_size)
-    draw = ImageDraw.Draw(summary_img)
-    draw.text((0, 0), summary, font=font)
-    enh = ImageEnhance.Contrast(summary_img)
-    enh.enhance(1.99).save(GENERATED_DIR + 'enh.ppm')
+    def replace_banner(self, currently_icon, summary):
 
-    enhanced_summary = Image.open(GENERATED_DIR + 'enh.ppm')
+        calendar_text = get_calendar_data()
+        calendar_text+= "  "
+        message_text = get_message_data()
+        font_size_in_points = 9
+        font = ImageFont.truetype(FONTS_DIR + 'led.ttf', font_size_in_points)
+        font_size = font.getsize(summary)
+        print('summary: {}, font size: {} calendar_text: {}, message_text{}'.format(summary, font_size, calendar_text, message_text))
+        summary_img = Image.new('RGB', font_size)
+        draw = ImageDraw.Draw(summary_img)
+        draw.text((0, 0), summary, font=font)
+        enh = ImageEnhance.Contrast(summary_img)
+        enh.enhance(1.99).save(GENERATED_DIR + 'enh.ppm')
 
-    current_img = Image.open('{}{}'.format(IMAGES_DIR, weather_files[currently_icon]))
+        enhanced_summary = Image.open(GENERATED_DIR + 'enh.ppm')
 
-    if message_text:
-       font_size = font.getsize(message_text)
-       message_img = Image.new('RGB', font_size)
-       message_draw = ImageDraw.Draw(message_img)
-       message_draw.text((0, 0), message_text, font=font, fill='GreenYellow')
-       enh_message = ImageEnhance.Contrast(message_img)
-       enh_message.enhance(1.99).save(GENERATED_DIR + 'messagetext.ppm')
-       enhanced_message = Image.open(GENERATED_DIR + 'messagetext.ppm')
-       message_width = enhanced_message.width
-    else:
-       message_width = 0
+        current_img = Image.open('{}{}'.format(IMAGES_DIR, weather_files[currently_icon]))
 
-    font_size = font.getsize(calendar_text)
-    calendar_img = Image.new('RGB', font_size)
-    calendar_draw = ImageDraw.Draw(calendar_img)
-    calendar_draw.text((0, 0), calendar_text, font=font, fill='cyan')
-    enh_calendar = ImageEnhance.Contrast(calendar_img)
-    enh_calendar.enhance(1.99).save(GENERATED_DIR + 'calendartext.ppm')
+        if message_text:
+           font_size = font.getsize(message_text)
+           message_img = Image.new('RGB', font_size)
+           message_draw = ImageDraw.Draw(message_img)
+           message_draw.text((0, 0), message_text, font=font, fill='GreenYellow')
+           enh_message = ImageEnhance.Contrast(message_img)
+           enh_message.enhance(1.99).save(GENERATED_DIR + 'messagetext.ppm')
+           enhanced_message = Image.open(GENERATED_DIR + 'messagetext.ppm')
+           message_width = enhanced_message.width
+        else:
+           message_width = 0
 
-    enhanced_calendar = Image.open(GENERATED_DIR + 'calendartext.ppm')
+        font_size = font.getsize(calendar_text)
+        calendar_img = Image.new('RGB', font_size)
+        calendar_draw = ImageDraw.Draw(calendar_img)
+        calendar_draw.text((0, 0), calendar_text, font=font, fill='cyan')
+        enh_calendar = ImageEnhance.Contrast(calendar_img)
+        enh_calendar.enhance(1.99).save(GENERATED_DIR + 'calendartext.ppm')
+
+        enhanced_calendar = Image.open(GENERATED_DIR + 'calendartext.ppm')
 
 
-    size = (enhanced_summary.width + current_img.width + enhanced_calendar.width + message_width, 16)
+        size = (enhanced_summary.width + current_img.width + enhanced_calendar.width + message_width, 16)
 
-    banner = Image.new('RGB', size)
-    banner.paste(enhanced_summary, (0, 4))
-    banner.paste(current_img, (enhanced_summary.width, 0))
+        banner = Image.new('RGB', size)
+        banner.paste(enhanced_summary, (0, 4))
+        banner.paste(current_img, (enhanced_summary.width, 0))
 
-    colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
-    for i, color in enumerate(colors):
-       stripe = Image.new('RGB', (enhanced_summary.width, 1), color)
-       banner.paste(stripe, (0, i if i < 3 else i + 10))
-    banner.paste(enhanced_calendar, (enhanced_summary.width + current_img.width, 4))
+        colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+        for i, color in enumerate(colors):
+           stripe = Image.new('RGB', (enhanced_summary.width, 1), color)
+           banner.paste(stripe, (0, i if i < 3 else i + 10))
+        banner.paste(enhanced_calendar, (enhanced_summary.width + current_img.width, 4))
 
-    if message_text:
-       banner.paste(enhanced_message, (enhanced_summary.width + current_img.width + enhanced_calendar.width, 4))
+        if message_text:
+           banner.paste(enhanced_message, (enhanced_summary.width + current_img.width + enhanced_calendar.width, 4))
 
-    banner.save(GENERATED_DIR + 'weather.ppm')
-    banner.save(STATIC_DIR + 'weather.ppm')
-    exportJpg(GENERATED_DIR + 'weather.ppm', STATIC_DIR + 'display.jpg')
+        banner.save(GENERATED_DIR + 'weather.ppm')
+        banner.save(STATIC_DIR + 'weather.ppm')
+        self.exportJpg(GENERATED_DIR + 'weather.ppm', STATIC_DIR + 'display.jpg')
+
+    @staticmethod
+    def exportJpg(ppmFilePath, outputFilePath):
+        im = Image.open(ppmFilePath)
+        im.save(outputFilePath)
 
 
 def get_calendar_data():
@@ -135,15 +142,11 @@ def get_message_data():
     # return message + " "
 
 
-def exportJpg(ppmFilePath,outputFilePath):
-    im = Image.open(ppmFilePath)
-    im.save(outputFilePath)
-
-
 if __name__ == '__main__':
     load_dotenv(find_dotenv())
     API_KEY = os.environ['DARK_SKY_API_KEY']
     LAT = os.environ['LAT']
     LONG = os.environ['LONG']
     wi = WeatherInfo(api_key=API_KEY, lat=LAT, long=LONG)
-    replace_banner(**wi.get_weather())
+    bm = BannerMaker()
+    bm.replace_banner(**wi.get_weather())
