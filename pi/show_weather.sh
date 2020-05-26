16f662082004ad197025e932edf08e51e61509bd
Brad Dettmer <11279144+redSlug@users.noreply.github.com>
2020-05-25 22:43:39 -0400

    add log rotation

--- pi/show_weather.sh
+++ pi/show_weather.sh
@@ -1,39 +1,60 @@
 #!/bin/bash
 
-# crontab
-# @reboot /home/pi/weather-reporter/pi/show_weather.sh >> /home/pi/weather-reporter/log
+# crontab -e
+# @reboot /home/pi/weather-reporter/pi/show_weather.sh
+# sed -i 's/weather.ppm/weather_2.ppm/g' /home/pi/weather-reporter/pi/show_weather.sh
 
-cleanup() {
-    echo "Cleaning stuff up $(date)" >> log
+MAX_LOG_FILE_SIZE=250000000
+LOG_FILE=/home/pi/weather-reporter/pi/log
+BANNER_IMAGE_FILE="weather.ppm"
+POLLING_DELAY=2m
+LED_DELAY_MS=18
+
+
+function log_to_file {
+    echo "$(date) $1" >> log
+}
+
+function cleanup {
+    log_to_file "Clearing LED display"
     sudo pkill demo
     exit
 }
 
-echo hi
+function rotate_logs_if_needed {
+    log_file_size=$(du -b log | tr -s '\t' ' ' | cut -d' ' -f1)
 
-pushd /home/pi/weather-reporter/pi
-image_file="weather.ppm"
-if [ ! -f $image_file ]; then
-    echo "File not found! $(date)" >> log
-    exit 1
-fi
+    if [ $log_file_size -gt $MAX_LOG_FILE_SIZE ];then
+        log_to_file "Rotating log file of size $log_file_size bytes"
+        mv $LOG_FILE "$LOG_FILE.backup"
+        touch $LOG_FILE
+    fi
+}
 
-delay_milliseconds=18
+function main {
+    pushd /home/pi/weather-reporter/pi
+
+    if [ ! -f $BANNER_IMAGE_FILE ]; then
+        log_to_file "File not found!"
+        exit 1
+    fi
+
+    while true; do
+        wget -N http://206.189.229.207/static/$BANNER_IMAGE_FILE
+        if [ $? -eq 0 ];
+        then
+            log_to_file "wget succeeded"
+            sudo pkill demo
+            sudo rpi-rgb-led-matrix/examples-api-use/demo -D 1 $BANNER_IMAGE_FILE --led-no-hardware-pulse --led-rows=16 --led-cols=32 -m $LED_DELAY_MS --led-daemon --led-brightness=10
+        else
+            log_to_file "wget failed - file was likely not modified"
+        fi
+        rotate_logs_if_needed
+        sleep $POLLING_DELAY
+    done
+
+    popd
+}
 
 trap cleanup EXIT
-while true; do
-    wget -N http://206.189.229.207/static/$image_file
-    if [ $? -eq 0 ];
-    then
-        echo "wget succeeded $(date)"
-        sudo pkill demo
-	    sudo rpi-rgb-led-matrix/examples-api-use/demo -D 1 $image_file --led-no-hardware-pulse --led-rows=16 --led-cols=32 -m $delay_milliseconds --led-daemon --led-brightness=10
-    else
-        echo "wget failed - file was likely not modified $(date)"
-    fi
-    sleep 2m
-done
-
-echo bye
-
-popd
+main
